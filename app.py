cc79a2790bbabadfc3ab6151c428dd620063c5b5
William <william@Infinity.local>
2019-03-06 15:00:28 -0500

    2nd

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-def save_user(**user):
-    print(user)
-    for x in user:
-        print(x, user[x])
+def multiply(*numbers):
+    product = 1
+    for number in numbers:
+        product *= number
+    return product
 
 
-save_user(a=1, b=2, c='xyz')
-message = "a"
-
-
-def greet(name):
-    message = "b"
-
-
-def greet2(name):
-    global message
-    message = "c"
-
-
-greet("Tao")
-print("message =", message)
-greet2("Xu")
-print("message =", message)
+print("Start...")
+print(multiply(1, 2, 3))
