5d010784ec9d28318991bbd428af898cca1f3a55
William <william@Infinity.local>
2019-03-06 20:06:12 -0500

    20:06 03/06/2019

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-zeros = [0] * 10
-print(zeros)
-matrix = [[1, 2], [3, 4]]
-print(matrix)
-print(matrix[0])
-print(matrix[1][1])
-print(type(matrix))
-combined = zeros + matrix
-print(combined)
-print(zeros, matrix)
-numbers = list(range(10))
-print(numbers)
-chars = list("Hello World")
-print(chars)
-print(chars[0:])
-print(chars[:-1])
-print(chars[::2])
-print(list(range(20)))
-x, y, *z = chars
-print(x, y, z)
-print(type(z))
-x, *y, z = chars
-print(x, y, z)
+letters = ('x', "y", 'z')  # ['a', "b", 'c']
+for letter in enumerate(letters):
+    print(letter, letter[0], letter[1])
+for index, letter in enumerate(letters):
+    print(index, letter)
+x = [1, 2]
+x1, x2 = x
+print(x, x1, x2)
+y = (3, 4)
+y1, y2 = y
+print(y, y1, y2)
