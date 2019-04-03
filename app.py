9ed881a027f3a97a6e934362d7947f70dfaf687b
Tao <nytaoxu@gmail.com>
2019-04-03 15:06:54 -0400

    python set

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from array import array
+numbers = [1, 1, 1, 2, 3, 3, 4, 5]
+first = set(numbers)
+second = set([1, 3, 7, 8, 9])
+print(first, second)
+print(first | second)
+print(first & second)
+print(first - second)
+print(first ^ second)
 
-x = array('i', [1, 2, 3])
+x = set([])
 print(type(x))
-print(x[2])
-x[2] = 1.5
+x.add(1)
+x.add(1.0)
+x.add('1.0')
+x.add('1')
+print(x)
+x.remove(1.0)
+print(x)
+for item in x:
+    print(item)
