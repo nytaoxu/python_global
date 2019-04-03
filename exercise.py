9f5772ea6ea4a7b928e6ff3b0394183470ae2b39
Tao <nytaoxu@gmail.com>
2019-04-03 16:01:54 -0400

    exercise: the most common character

---
+++ exercise.py
@@ -0,0 +1,21 @@
+sentence = "This is a common interview question"
+dict = {}
+for letter in sentence:
+    if letter == ' ':
+        continue
+    if letter in dict:
+        dict[letter] += 1
+    else:
+        dict[letter] = 1
+
+# print("dict[" "] =", dict.get(" ", None))
+
+result_key = ""
+result_value = -1
+for key, value in dict.items():
+    if value > result_value:
+        result_key = key
+        result_value = value
+
+print("The most common character is", result_key,
+      ", and it appears", result_value, "times.")
